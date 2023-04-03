f5f2a4f31acf9f66f9127f441e7848ba2b367ce7
ISADORA PALÚ CORRÊA <isadora_p_correa@estudante.sesisenai.org.br>
2023-04-03 09:18:20 -0300

    Initial commit

---
+++ app/src/main/java/com/example/jogorandom/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.jogorandom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    EditText entrada;
+    TextView saida;
+    int numeroGerado = 0;
+    int tentativas = 0;
+    Random gerador = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        entrada = findViewById(R.id.entrada);
+        saida = findViewById(R.id.saida);
+    }
+    public void gerar(View v){
+        tentativas = 0;
+        numeroGerado = gerador.nextInt(100)+1;
+    }
+    public void confirmar(View v){
+        tentativas ++;
+        String mensagem = " ";
+        int digito = Integer.parseInt(entrada.getText().toString());
+        if(tentativas == 5){
+            mensagem = "Suas tentativas se esgotaram!";
+        }else{if(digito>numeroGerado){
+            mensagem = "O número gerado é menor!\nVocê ainda tem "+(5-tentativas)+" tentativas.";
+        } else if(digito<numeroGerado){
+            mensagem = "O número gerado é maior!\nVocê ainda tem "+(5-tentativas)+" tentativas.";
+        }else{
+            mensagem = "Parabéns, você acertou!\nForam gastas "+(5-tentativas)+" tentativas.";
+        }}
+        saida.setText(mensagem);
+
+    }
+}
